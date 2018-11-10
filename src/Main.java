d883736b086e3aa40c0854a5478bf5c75c7c4fbc
jgarciapft <jgarciapft@alumnos.unex.es>
2018-11-10 17:56:26 +0100

    Clase Main:     - Implementada la lectura de los nombres de las ciudades

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,17 @@ public class Main {
     public static void main(String[] args) {
         Lector lector = new Lector();                           // Lector del flujo de entrada estándar
         Escritor escritor = new Escritor();                     // Escritor del flujo de salida estándar
+
+        int n;                                                  // Número de ciudades del mapa
+        int d;                                                  // Número de carreteras del mapa
+        int p;                                                  // Número de preguntas
+        GrafoNDVIndexCad mapaCarreteras;                        // Grafo del mapa de carreteras (vértices -> ciudades)
+
+        n = lector.leerNumero();                                // Lee el número de ciudades del mapa
+        mapaCarreteras = new GrafoNDVIndexCad(n);               // Inicializa el mapa con las ciudades iniciales
+        for (int i = 0; i < n; i++) {                           // Añade las ciudades al mapa
+            mapaCarreteras.añadirCiudad(lector.leerLinea());
+        }
     }
 
 }
